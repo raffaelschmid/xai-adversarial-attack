571130a249ac4f73aa2f73b4d07ae7587213e642
Raffael Schmid <raffael.schmid@cloudarchitects.ch>
2021-11-30 06:46:06 +0100

    baseline dependencies

---
+++ Makefile
@@ -0,0 +1,4 @@
+SHELL = /bin/bash
+
+test:
+	python3 setup.py test
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xai-adversarial-attack',
+    version='1.0.0',
+    url='https://github.com/raffaelschmid/xai-adversarial-attack',
+    license='',
+    author='Raffael Schmid',
+    author_email='raffael.schmid@cloudarchitects.ch',
+    description='Explainable AI - Adversarial Attacks',
+    zip_safe=True,
+    packages=find_packages(),
+)
