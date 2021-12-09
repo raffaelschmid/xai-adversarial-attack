a8b2eac483ded5e3eacb6609067258a40c932873
Raffael Schmid <raffael.schmid@cloudarchitects.ch>
2021-12-09 07:06:32 +0100

    analyze adversarial images

--- data/load.py
+++ data/load.py
@@ -3,19 +3,24 @@ from pandas import read_parquet, Series
 from data import file
 
 
-def load_train_dataset():
+def load_train_dataset(normalize=False):
     x, y = load_train_data()
-    x, y = _reshape_input(x, y, normalize=False)
-    return (x, y)
+    x, y = _reshape_input(x, y)
+    if normalize:
+        x = _normalize(x)
+    return x, y
 
 
 def load_train_data():
     return _load_data(file.data_train)
 
-def load_test_dataset():
+
+def load_test_dataset(normalize=False):
     x, y = load_test_data()
-    x, y = _reshape_input(x, y, normalize=False)
-    return (x, y)
+    x, y = _reshape_input(x, y)
+    if normalize:
+        x = _normalize(x)
+    return x, y
 
 
 def load_test_data():
@@ -25,11 +30,14 @@ def load_test_data():
 def _load_data(path):
     data = read_parquet(path)
     images = data.image
-    return (images, data.label)
+    return images, data.label
 
-def _reshape_input(x, y, normalize=True):
+
+def _reshape_input(x, y):
     no_elements = x.shape[0]
     images = x.apply(Series).stack().to_numpy().reshape(no_elements, 28, 28, 1)
-    if normalize:
-        images /= 255
-    return (images, y)
+    return images, y
+
+
+def _normalize(x):
+    return x / 255
